ebbea63539667bded3c559f224cb97f8d5c4afd1
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-09 12:32:08 +0530

    Added: Welcome to gamble

---
+++ src/com/gamblingsimulator/GamblingSimulUC1.java
@@ -0,0 +1,11 @@
+package com.gamblingsimulator;
+
+public class GamblingSimulUC1 {
+    	public static void main(String[] args) {
+	       int Stake = 100;
+	       int Bet = 1;
+	    System.out.println("welcome to the gamble world with every day stake is");
+	}
+
+}
+ 
---
+++ src/com/gamblingsimulator/Gamblingsimul.java
@@ -0,0 +1,10 @@
+package com.gamblingsimulator;
+
+public class Gamblingsimul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("welcome to gambling");
+	}
+
+}

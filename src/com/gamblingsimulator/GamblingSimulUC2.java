c7943177aff5ebc07a0c45285e50fb902d036346
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-09 12:51:52 +0530

    Added:Gambling win or loose

---
+++ src/com/gamblingsimulator/GamblingSimulUC2.java
@@ -0,0 +1,17 @@
+package com.gamblingsimulator;
+
+public class GamblingSimulUC2 {
+      public static void main(String[] args) {
+    	  int stake = 100;
+    	  int bet = 1;
+    	  int TRIAL = (int)Math.floor(Math.random()*2);
+    	  if(TRIAL == 0) {
+    		  stake++;
+    		  System.out.println("Gambler won the game" + stake);
+    	  }else {
+    		  stake--;
+    		  System.out.println("Gambler loose the game " + stake);
+    		  System.out.println("Gambler having stake after bet are of $" + stake);
+    	  }
+        }
+    }
